b6109afc46fa46cb23f4db5f7ad84cd29f1180d8
professorkang <103405363+professorkang@users.noreply.github.com>
2022-05-27 15:59:08 +0900

    Add files via upload

---
+++ 038_EcgPpt/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace _038_EcgPpt
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 필수 디자이너 변수입니다.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 사용 중인 모든 리소스를 정리합니다.
+    /// </summary>
+    /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form 디자이너에서 생성한 코드
+
+    /// <summary>
+    /// 디자이너 지원에 필요한 메서드입니다. 
+    /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.ch = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.viewAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.autoScrollToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.dataCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.dataCountToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+      ((System.ComponentModel.ISupportInitialize)(this.ch)).BeginInit();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // ch
+      // 
+      chartArea3.Name = "ChartArea1";
+      this.ch.ChartAreas.Add(chartArea3);
+      this.ch.Dock = System.Windows.Forms.DockStyle.Fill;
+      legend3.Name = "Legend1";
+      this.ch.Legends.Add(legend3);
+      this.ch.Location = new System.Drawing.Point(0, 24);
+      this.ch.Name = "ch";
+      series3.ChartArea = "ChartArea1";
+      series3.Legend = "Legend1";
+      series3.Name = "Series1";
+      this.ch.Series.Add(series3);
+      this.ch.Size = new System.Drawing.Size(800, 426);
+      this.ch.TabIndex = 0;
+      this.ch.Text = "chart1";
+      this.ch.Click += new System.EventHandler(this.ch_Click);
+      this.ch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ch_MouseClick);
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewAllToolStripMenuItem,
+            this.autoScrollToolStripMenuItem,
+            this.exitToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+      this.menuStrip1.TabIndex = 1;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // viewAllToolStripMenuItem
+      // 
+      this.viewAllToolStripMenuItem.Name = "viewAllToolStripMenuItem";
+      this.viewAllToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+      this.viewAllToolStripMenuItem.Text = "View All";
+      this.viewAllToolStripMenuItem.Click += new System.EventHandler(this.viewAllToolStripMenuItem_Click);
+      // 
+      // autoScrollToolStripMenuItem
+      // 
+      this.autoScrollToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataCountToolStripMenuItem,
+            this.dataCountToolStripMenuItem1});
+      this.autoScrollToolStripMenuItem.Name = "autoScrollToolStripMenuItem";
+      this.autoScrollToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+      this.autoScrollToolStripMenuItem.Text = "Auto Scroll";
+      this.autoScrollToolStripMenuItem.Click += new System.EventHandler(this.autoScrollToolStripMenuItem_Click);
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+      this.exitToolStripMenuItem.Text = "Exit";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+      // 
+      // dataCountToolStripMenuItem
+      // 
+      this.dataCountToolStripMenuItem.Name = "dataCountToolStripMenuItem";
+      this.dataCountToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.dataCountToolStripMenuItem.Text = "DataCount++";
+      this.dataCountToolStripMenuItem.Click += new System.EventHandler(this.dataCountToolStripMenuItem_Click);
+      // 
+      // dataCountToolStripMenuItem1
+      // 
+      this.dataCountToolStripMenuItem1.Name = "dataCountToolStripMenuItem1";
+      this.dataCountToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+      this.dataCountToolStripMenuItem1.Text = "DataCount--";
+      this.dataCountToolStripMenuItem1.Click += new System.EventHandler(this.dataCountToolStripMenuItem1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.ch);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.ch)).EndInit();
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataVisualization.Charting.Chart ch;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem viewAllToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem autoScrollToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem dataCountToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem dataCountToolStripMenuItem1;
+  }
+}
+
---
+++ 038_EcgPpt/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _038_EcgPpt
+{
+  public partial class Form1 : Form
+  {
+    private double[] ecg = new double[100000];
+    private double[] ppg = new double[100000];
+    private int ecgCount; // ecg 데이터 갯수
+    private int ppgCount; // ppg 데이터 갯수
+    private Timer t = new Timer();
+    public Form1()
+    {
+      InitializeComponent();
+      this.Text = "ECG/PPG";
+      this.WindowState = FormWindowState.Maximized;
+
+      EcgRead();
+      PpgRead();
+
+      ChartSetting();
+
+      t.Interval = 10; // 0.01초
+      t.Tick += T_Tick;
+    }
+
+    int cursorX = 0;  // 디스플레이되는 데이터의 시작점
+    bool scrolling = false; // true 일 때 스크롤
+
+    private void T_Tick(object sender, EventArgs e)
+    {
+      // 한 화면에 500개의 데이터를 보여줄 예정
+      if (cursorX + dataCount < ecgCount)
+        ch.ChartAreas[0].AxisX.ScaleView.Zoom(
+          cursorX, cursorX + dataCount);
+      else
+        t.Stop();
+      cursorX += 2;
+    }
+
+    private void ChartSetting()
+    {
+      // 커서 사용 가능
+      ch.ChartAreas[0].CursorX.IsUserEnabled = true; 
+      // zoom 범위 선택 가능
+      ch.ChartAreas[0].CursorX.IsUserSelectionEnabled
+        = true;
+
+      // chart 영역 설정
+      ch.ChartAreas[0].BackColor = Color.Black;
+      ch.ChartAreas[0].AxisX.Minimum = 0;
+      ch.ChartAreas[0].AxisX.Maximum = ecgCount;
+      ch.ChartAreas[0].AxisX.Interval = 50;
+      ch.ChartAreas[0].AxisX.MajorGrid.LineColor =
+        Color.Gray;
+      ch.ChartAreas[0].AxisX.MajorGrid.LineDashStyle =
+        ChartDashStyle.Dash;
+
+      ch.ChartAreas[0].AxisY.Minimum = -2;
+      ch.ChartAreas[0].AxisY.Maximum = 6;
+      ch.ChartAreas[0].AxisY.Interval = 0.5;
+      ch.ChartAreas[0].AxisY.MajorGrid.LineColor =
+        Color.Gray;
+      ch.ChartAreas[0].AxisY.MajorGrid.LineDashStyle =
+        ChartDashStyle.Dash;
+
+      // Series 2개 새로 만들기
+      ch.Series.Clear();
+      ch.Series.Add("ECG");
+      ch.Series.Add("PPG");
+
+      ch.Series["ECG"].ChartType = SeriesChartType.Line;
+      // ch.Series[0].ChartType = SeriesChartType.Line;
+      ch.Series["ECG"].Color = Color.Orange;
+      ch.Series["ECG"].BorderWidth = 2;
+      ch.Series["ECG"].LegendText = "ECG";
+
+      ch.Series["PPG"].ChartType = SeriesChartType.Line;
+      // ch.Series[1].ChartType = SeriesChartType.Line;
+      ch.Series["PPG"].Color = Color.LightGreen;
+      ch.Series["PPG"].BorderWidth = 2;
+      ch.Series["PPG"].LegendText = "PPG";
+
+      // 데이터를 series에 추가
+      foreach(var v in ecg)
+      {
+        ch.Series["ECG"].Points.Add(v);
+      }
+      foreach (var v in ppg)
+      {
+        ch.Series["PPG"].Points.Add(v);
+      }
+    }
+
+    private void PpgRead()
+    {
+      string fileName = "../../Data/ppg.txt";
+      string[] lines = File.ReadAllLines(fileName);
+
+      double min = double.MaxValue;
+      double max = double.MinValue;
+
+      int i = 0;
+      foreach (var line in lines)
+      {
+        ppg[i] = double.Parse(line);
+        if (ppg[i] < min) min = ppg[i];
+        if (ppg[i] > max) max = ppg[i];
+        i++;
+      }
+      ppgCount = i;
+      string s = string.Format(
+        "PPG: Count={0}, min={1}, max={2}",
+        ppgCount, min, max);
+      MessageBox.Show(s);
+    }
+
+    private void EcgRead()
+    {
+      string fileName = "../../Data/ecg.txt";
+      string[] lines = File.ReadAllLines(fileName);
+
+      double min = double.MaxValue;
+      double max = double.MinValue;
+
+      int i = 0;
+      foreach(var line in lines)
+      {
+        ecg[i] = double.Parse(line) + 3;
+        if(ecg[i] < min) min = ecg[i];
+        if(ecg[i] > max) max = ecg[i];
+        i++;
+      }
+      ecgCount = i;
+      string s = string.Format(
+        "ECG: Count={0}, min={1}, max={2}",
+        ecgCount, min, max);
+      MessageBox.Show(s);
+    }
+
+    private void autoScrollToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      t.Start();
+      scrolling = true;
+    }
+
+    private void viewAllToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      ch.ChartAreas[0].AxisX.ScaleView.Zoom(0, ecgCount);
+      t.Stop();
+      scrolling = false;
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+    bool isTimerRunning = true;
+    private int dataCount = 500;
+
+    private void ch_Click(object sender, EventArgs e)
+    {
+      if(isTimerRunning)
+      {
+        t.Stop();
+        isTimerRunning = false;
+      }
+      else
+      {
+        t.Start();
+        isTimerRunning = true;
+      }
+    }
+
+    private void ch_MouseClick(object sender, MouseEventArgs e)
+    {
+      HitTestResult htr = ch.HitTest(e.X, e.Y);
+      if(htr.ChartElementType == ChartElementType.DataPoint)
+      {
+        t.Stop();
+        string s = string.Format(
+          "Count: {0}, ECG: {1}, PPG:{2}",
+          htr.PointIndex,
+          ch.Series["ECG"].Points[htr.PointIndex].YValues[0],
+          ch.Series["PPG"].Points[htr.PointIndex].YValues[0]);
+        MessageBox.Show(s);
+      }
+    }
+
+    private void dataCountToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      dataCount = dataCount*2;
+    }
+
+    private void dataCountToolStripMenuItem1_Click(object sender, EventArgs e)
+    {
+      dataCount = dataCount / 2;
+    }
+  }
+}
